df359690b74bf6a6d2fd59adc6bf6b581aeb5720
Giuseppe Scoppino <scoppino.giuseppe@gmail.com>
2016-02-16 20:14:26 -0500

    use module key instead of string in bootstraper.

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import angular from 'angular';
 import app from './app/app';
 
 const $injector = angular
-    .bootstrap(window.document, ['app'], { strictDi: true });
+    .bootstrap(window.document, [app.name], { strictDi: true });
 
 export default $injector;
